41dee7093e2f763d7bdd3a5641735181426b3b20
ankitpasvan208@gmail.com <youremail@example.com>
2025-09-13 00:18:51 +0530

    change in auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,33 @@
+
+const bcrypt=require("bcrypt");
+const User=require("../models/User");
+
+// signup route handler
+exports.signup=async(req,res)=>{
+    try{
+        // get data 
+        const {name,email,password,role}=req.body;
+        // check if user already exist
+        const existingUser=await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({
+                success:false,
+                message:`User already Exists`,
+            })
+        }
+        // secure password
+        let hashedPassword;
+        try{
+            hashedPassword=await bcrypt.hash(password,10);
+        }
+        catch(err){
+            return res.status(500).json({
+                success:false,
+                message:'error in hashing function'
+            })
+        }
+    }
+    catch(error){
+
+    }
+}
