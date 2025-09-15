e6fe92489728667c5c921056025a478e706a35f3
ankitpasvan208@gmail.com <youremail@example.com>
2025-09-16 00:48:04 +0530

    change in signup

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,6 @@ const router= express.Router();
 const{login,signup}=require("../controllers/auth");
 
 // router.post("/login",login);
-router.post("signup",signup);
+router.post("/signup",signup);
 
 module.exports=router;
